c6e314ead1e83b49565e1607dbe60a967751e2c1
otasevicandrija <38298634+otasevicandrija@users.noreply.github.com>
2019-06-12 11:16:01 -0400

    Adding one more property
    Added property for Security module

--- python/webdriverScreenShot_ftpUpload/models/ftpConfiguration.py
+++ python/webdriverScreenShot_ftpUpload/models/ftpConfiguration.py
@@ -1,11 +1,7 @@
 class FTPConfiguration(object):
-    server = ""
-    user = ""
-    password = ""
-    ftpFolder = ""
-
-    def __init__(self, server, user, password, ftpFolder):
+    def __init__(self, server, user, password, ftpFolder, securityType):
         self.server = server
         self.user = user
         self.password = password
         self.ftpFolder = ftpFolder
+        self.securityType = securityType
